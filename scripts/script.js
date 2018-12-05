315888056abb7d7ad5fb0abc6f472f61e870eb73
Cikatros <bebertau18@gmail.com>
2018-12-05 02:13:20 +0100

    ajout des fonction move

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 var world = [
 	[0,0,0,1,0,0,0,0],
-	[0,0,0,1,1,0,0,0],
-	[0,0,0,1,0,0,0,0],
+	[0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0],
 	[0,0,0,0,0,0,0,0],
 	[0,0,0,0,0,0,0,0],
 	[0,0,0,0,0,0,0,0],
@@ -13,7 +13,11 @@ var world = [
 	[0,0,0,0,0,0,0,0],
 ];
 
-function drawSquare()
+/*======================================================================
+**  Nom         : drawForm
+**  Description :  
+**======================================================================*/
+function drawForm()
 {
     document.getElementById('world').innerHTML = "";
     
@@ -43,25 +47,25 @@ function drawSquare()
 function moveDown()
 {
     var canMove = true;
-    for(var y=0; y<world.length; y++)
-	{
-		for(var x=0; x<world[y].length; x++)
-		{
-           if(world[y][x] > 0 && world[y][x] < 10 ) 
+    for(var y=world.length-1; y>=0; y--) 
+    {
+        for(var x=0; x<world[y].length; x++) 
+        {
+            if(world[y][x] > 0 && world[y][x] < 10 )
             {
-                 if(y === world.length-1 || world[y+1][x] > 10)
-                 {
-                    canMove = false; 
+                if(y+1 === world.length || world[y+1][x] > 10)
+                {
+                    canMove = false;
                     stop();
-                 }  
+                }
             }
         }
     }
-    if(canMove)
+    if (canMove) 
     {
-        for(var y=world.length-1; y>=0; y--)
+        for(var y=world.length-1; y>=0; y--) 
         {
-            for(var x=0; x<world[y].length; x++)
+            for(var x=0; x<world[y].length; x++) 
             {
                 if(world[y][x] > 0 && world[y][x] < 10 )
                 {
@@ -69,6 +73,107 @@ function moveDown()
                     world[y][x] = 0;
                 }
             }
+        }
+        drawForm();
+    }
+    checkLines();
+}
+
+/*======================================================================
+**  Nom         : moveLeft
+**  Description :  
+**======================================================================*/
+function moveLeft()
+{
+    var canMove = true;
+    for(var y=world.length-1; y>=0; y--) 
+    {
+        for(var x=0; x<world[y].length; x++) 
+        {
+            if(world[y][x] > 0 && world[y][x] < 10 )
+            {
+                if(x === 0 || world[y][x-1] > 10)
+                {
+                    canMove = false;
+                }
+            }
+        }
+    }
+    if (canMove) 
+    {
+        for(var y=world.length-1; y>=0; y--) 
+        {
+            for(var x=0; x<world[y].length; x++) 
+            {
+                if(world[y][x] > 0 && world[y][x] < 10 )
+                {
+                    world[y][x-1] = world[y][x];
+                    world[y][x] = 0;
+                }
+            }
+        }
+        drawForm();
+    }
+}
+
+/*======================================================================
+**  Nom         : moveRight
+**  Description :  
+**======================================================================*/
+function moveRight()
+{
+    var canMove = true;
+    for(var y=world.length-1; y>=0; y--) 
+    {
+        for(var x=0; x<world[y].length; x++)
+        {
+            if(world[y][x] > 0 && world[y][x] < 10 )
+            {
+                if(x === 7 || world[y][x+1] > 10)
+                {
+                    canMove = false;
+                }
+            }
+        }
+    }
+    if (canMove) 
+    {
+        for(var y=world.length-1; y>=0; y--) 
+        {
+            for(var x=world[y].length; x>=0; x--) 
+            {
+                if(world[y][x] > 0 && world[y][x] < 10 )
+                {
+                    world[y][x+1] = world[y][x];
+                    world[y][x] = 0;
+                }
+            }
+        }
+        drawForm();
+    }
+}
+
+/*======================================================================
+**  Nom         : checkLines
+**  Description : 
+**======================================================================*/
+function checkLines()
+{
+    for(var y=world.length-1; y>=0; y--) 
+    {
+        var fullLine = true;
+        for(var x=0; x<world[y].length; x++) 
+        {
+            if(world[y][x] < 10) 
+            {
+                fullLine = false;
+            }
+        }
+        if (fullLine) 
+        {
+            world.splice(y, 1);
+            world.splice(0, 0, [0,0,0,0,0,0,0,0])
+            y++;
         }
     }
 }
@@ -92,6 +197,47 @@ function stop()
             }
         }
     }
+    checkLines();
+    var rand = Math.floor(Math.random()*7);
+    if(rand === 0)
+    {
+        world[0] = [0,0,0,1,1,0,0,0]
+        world[1] = [0,0,0,1,1,0,0,0]    
+    }
+    else if(rand === 1)
+    {
+        world[0] = [0,0,0,1,0,0,0,0]
+        world[1] = [0,0,0,1,0,0,0,0]
+        world[2] = [0,0,0,1,0,0,0,0]
+        world[3] = [0,0,0,1,0,0,0,0]
+    }
+    else if(rand === 2)
+    {
+        world[0] = [0,0,1,1,0,0,0,0]
+        world[1] = [0,0,0,1,1,0,0,0]
+    }
+    else if(rand === 3)
+    {
+        world[0] = [0,0,0,0,1,1,0,0]
+        world[1] = [0,0,0,1,1,0,0,0]   
+    }
+    else if(rand === 4)
+    {
+        world[0] = [0,0,0,1,0,0,0,0]
+        world[1] = [0,0,0,1,0,0,0,0]
+        world[2] = [0,0,0,1,1,0,0,0]
+    }
+    else if(rand === 5)
+    {
+        world[0] = [0,0,0,1,1,0,0,0]
+        world[1] = [0,0,0,1,0,0,0,0]
+        world[2] = [0,0,0,1,0,0,0,0] 
+    }
+    else if(rand === 6)
+    {
+        world[0] = [0,0,0,1,0,0,0,0]
+        world[1] = [0,0,1,1,1,0,0,0]   
+    }
 }
 
 /*======================================================================
@@ -102,15 +248,29 @@ function stop()
 function loop()
 {
     console.log('Boucle lancé');
-    drawSquare();
+    drawForm();
     moveDown();
     setTimeout(loop, 1000);
 }
 
-document.onkeydown = function(e)
+/*======================================================================
+**  Nom         : onkeydown
+**  Description : 
+**======================================================================*/
+document.onkeydown = function(e) 
 {
-    console.log(e);
+    console.log(e)
+    if (e.keyCode === 37)
+    {
+        moveLeft(); 
+    } else if (e.keyCode === 39)
+    {
+        moveRight();
+    } else if (e.keyCode === 40)
+    {
+        moveDown();
+    }
 }
 
-drawSquare();
+drawForm();
 loop();
